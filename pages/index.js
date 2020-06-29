124133668d3dd196738b1b509cdc6c0c57dec70d
gmahima <mahimagangavarapu@gmail.com>
2020-06-29 19:10:11 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Main lead in a hit anime/manga</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
